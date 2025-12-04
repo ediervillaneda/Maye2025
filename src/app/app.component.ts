6e444baef49463a6c64b3b24c4fa15e758b97f5f
Edier Andrés Villaneda Navarro <ediervillaneda@gmail.com>
2025-12-03 22:15:00 -0500

    feat: Initialize Angular application with `Animation2Component`, assets, and basic styling.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { CounterService } from './services/counter.service';
 import { AnimationComponent } from './pages/animation/animation.component';
 import { CountdownComponent } from './pages/countdown/countdown.component';
+import { Animation2Component } from './pages/animation2/animation2.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AnimationComponent, CountdownComponent],
+  imports: [AnimationComponent, CountdownComponent, Animation2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
